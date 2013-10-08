c23e3b098c3342bb47e52beb5ba8e9bf1fcdd3ff
A.Bekkine <devnull@localhost>
2013-10-08 11:56:28 +0300

    - Basic menu options display implemented.

--- inc/menu.h
+++ inc/menu.h
@@ -1,6 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <font.h>
 #include <layer.h>
 
 class Menu : public Layer
@@ -10,12 +11,21 @@ class Menu : public Layer
         ~Menu();
         bool Init( Volume& viewport );
         void Render();
+        void Resize(int width, int height);
         void ProcessCommand( Event::CommandType cmdCode );
 
     private:
         void Defaults();
         void RenderOptions();
 
+
+    private:
+        int _font_height;
+        int _line_step;
+        Font* _font;
+        Vector2i _cursor;
+        Vector2i _origin;
+        std::vector< std::string > _options;
 };
 
 #endif
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,24 +1,49 @@
 #include <GL/gl.h>
 
+#include <string>
+
 #include <gamestate.h>
 #include <menu.h>
 
 Menu::Menu()
 {
+    Defaults();
 }
 
 Menu::~Menu()
 {
 }
 
+void Menu::Defaults()
+{
+    _font = 0;
+    _font_height = 20;
+    _line_step = 10;
+
+    _origin.x = 10;
+    _origin.y = 10+_font_height;
+
+    _cursor = _origin;
+}
+
 bool Menu::Init(Volume& viewport)
 {
     bool result = true;
 
+    _use_screen_coords = true;
     Layer::Init( viewport );
 
-    _zoom_factor = 1.0;
-    _pan_factor = 1.0;
+    _zoom_factor = 0.0;
+    _pan_factor = 0.0;
+
+    _font = new Font();
+    _font->Size(_font_height);
+
+    result = _font->Init();
+
+    _options.clear();
+    _options.push_back("START");
+    _options.push_back("EXIT");
 
     return result;
 }
@@ -28,6 +53,11 @@ void Menu::Render()
     PreRender();
     RenderOptions();
     PostRender();
+}
+
+void Menu::Resize(int width, int height)
+{
+    Layer::Resize(width, height);
 }
 
 void Menu::ProcessCommand(Event::CommandType cmdCode)
@@ -49,23 +79,15 @@ void Menu::ProcessCommand(Event::CommandType cmdCode)
     }
 }
 
-void Menu::Defaults()
-{
-}
-
 void Menu::RenderOptions()
 {
-    const double SIZE = 0.1;
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslated(0.0, 0.0, 0.0);
-    glColor3d( 1.0, 1.0, 1.0 );
-    glBegin( GL_QUADS );
-    glVertex2d( -SIZE, -SIZE );
-    glVertex2d( -SIZE,  SIZE );
-    glVertex2d(  SIZE,  SIZE );
-    glVertex2d(  SIZE, -SIZE );
-    glEnd();
-    glPopMatrix();
+    std::vector< std::string >::iterator iOption;
+
+    _cursor = _origin;
+    for(iOption=_options.begin(); iOption!=_options.end(); ++iOption)
+    {
+        _font->Print( _cursor.x, _cursor.y, *iOption );
+        _cursor.y += _font_height+_line_step;
+    }
 }
 
