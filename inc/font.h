472c011f86aa4437fb6e8460b3a5a6e9d5a4cbce
A.Bekkine <devnull@localhost>
2013-10-14 11:15:14 +0300

    - Bounding box retrieval function added to font class.

--- inc/font.h
+++ inc/font.h
@@ -13,18 +13,24 @@ class Font
         ~Font();
         bool Init();
         void SetColor( double* color );
+        void Box( std::string text );
+        double Width();
+        double Height();
         void Size( int size );
         void Face( std::string name );
         void Print( int x, int y, std::string text );
 
     private:
         // Members
+        FTBBox _box;
         FTGLPixmapFont* _font;
         double _font_color[4];
         std::string _font_name;
         std::string _font_path;
         std::string _font_folder;
         int _font_size;
+        double _width;
+        double _height;
 };
 
 #endif
--- src/font.cpp
+++ src/font.cpp
@@ -11,6 +11,9 @@ Font::Font()
     _font_folder = Config::Instance()->font_folder;
     _font_size = Config::Instance()->font_size;
 
+    _width = -1.0;
+    _height = -1.0;
+
     _font_color[0] = 1.0;
     _font_color[1] = 1.0;
     _font_color[2] = 1.0;
@@ -54,6 +57,23 @@ void Font::Size( int size ) {
 void Font::Face( std::string name )
 {
     _font_name = name;
+}
+
+void Font::Box( std::string text )
+{
+    _box = _font->BBox( text.c_str() );
+    _width = _box.Upper().X() - _box.Lower().X();
+    _height = _box.Upper().Y() - _box.Lower().Y();
+}
+
+double Font::Width()
+{
+    return _width;
+}
+
+double Font::Height()
+{
+    return _height;
 }
 
 void Font::Print( int x, int y, std::string text )
